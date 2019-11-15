378d3b5060ccc1dcfe3a1fee4bcfb7534bda81e0
ekdnjs33 <gwi02029@gmail.com>
2019-11-15 12:13:36 +0900

    Prototype
    Prototype with scene transitions

---
+++ Team12 Project/Assets/Scenes/pauseBtn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseBtn : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject Button;
+ 
+    // Use this for initialization
+    void Start () {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void pausePlay() {
+            if(GameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+    }
+
+    void Resume() {
+        pauseMenuUI.SetActive(false);
+        Button.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Button.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
