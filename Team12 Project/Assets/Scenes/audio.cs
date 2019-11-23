96c526a8470e043aff326fef57bfe62a847093c2
ekdnjs33 <gwi02029@gmail.com>
2019-11-24 03:42:17 +0900

    create Mission panel

---
+++ Team12 Project/Assets/Scenes/audio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class audio : MonoBehaviour
+{
+    public static audio Instance;
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("playGame"))
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
