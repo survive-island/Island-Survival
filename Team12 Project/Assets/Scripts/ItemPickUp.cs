2c0dd8a8767b273a268b28e8464ac5549dd38a11
ykhaam <yerin57@naver.com>
2019-11-25 16:47:34 +0900

    refactored assets

---
+++ Team12 Project/Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    void PickUp()
+    {
+        Debug.Log("Picking up item.");
+        Destroy(gameObject);
+    }
+}
---
+++ Team12 Project/Assets/Scripts/navigate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navigate : MonoBehaviour
+{
+    public int playerSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))    {
+        	transform.position = transform.position + Camera.main.transform.forward * playerSpeed *        
+                 Time.deltaTime;
+     }
+    }
+}
