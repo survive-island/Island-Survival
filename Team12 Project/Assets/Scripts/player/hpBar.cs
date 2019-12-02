aecb4f5d612c2934368773f40faccf7719114adc
hope <thakd1011@naver.com>
2019-12-02 10:30:35 +0900

    make hp bar

---
+++ Team12 Project/Assets/Scripts/player/hpBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpBar : MonoBehaviour
+{
+    Image healthBar;
+    public Text healthText;
+    float maxHealth = 100f;
+    public static float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GetComponent<Image> ();
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+        healthText.text = ((int)(health)).ToString();
+        UnityEngine.Debug.Log(healthText.text);
+    }
+}
