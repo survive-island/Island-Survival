34f58d622f4fa933ea489d0e509775ac9f3cfc36
ykhaam <yerin57@naver.com>
2019-12-06 18:14:32 +0900

    put load button on the main scene

---
+++ Team12 Project/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    public void LoadingScene(string sceneName)
+    {
+        skipBtn.missionNum = 2;
+        SceneManager.LoadScene(sceneName);
+        inventory.Load();
+    }
+}
