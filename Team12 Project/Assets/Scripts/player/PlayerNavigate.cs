5d0e08081505ef53fbb2d167d7a0e2bf949df546
hope <thakd1011@naver.com>
2019-12-08 16:27:40 +0900

    modify boundary and add rain effect

--- Team12 Project/Assets/Scripts/player/PlayerNavigate.cs
+++ Team12 Project/Assets/Scripts/player/PlayerNavigate.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class PlayerNavigate : MonoBehaviour
 {
     public int playerSpeed = 2;
+    public GameObject rainEffect;
+
     Vector3[] startPosition = {
         new Vector3 (21.0f, 153.0f, -122.0f),
         new Vector3 (-208.0f, 17.0f, 453.0f),
-        new Vector3 (-204.0f, 33.0f, 317.0f),
+        // new Vector3 (-204.0f, 33.0f, 317.0f),
+        new Vector3 (-160.3791f, 62.60879f, 207.8396f),
     };
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = startPosition[skipBtn.missionNum % 3];
+        if(skipBtn.missionNum == 2) {
+            rainEffect.SetActive(true);
+        }else {
+            rainEffect.SetActive(false);
+        }
     }
 
     // Update is called once per frame
