b82d29aeaa3472bc777c9cb894c42133931039cd
ykhaam <yerin57@naver.com>
2019-12-09 01:20:41 +0900

    font changed

--- Team12 Project/Assets/Scripts/hintMessage.cs
+++ Team12 Project/Assets/Scripts/hintMessage.cs
@@ -8,6 +8,7 @@ public class hintMessage : MonoBehaviour
     public TextMeshProUGUI leveltext;
     public TextMeshProUGUI contentText;
 
+
     public void EasyText(GameObject panel)
     {
         Debug.Log("hint num : " + skipBtn.missionNum);
@@ -15,15 +16,15 @@ public class hintMessage : MonoBehaviour
         leveltext.text = "Easy Hint";
         if (skipBtn.missionNum == 1)
         {
-            contentText.text = "You must get a [wood] to build a tree house !!\n(You can get wood from: fence, board, log, crate) ";
+            contentText.text = "<color=black>You must get a [wood] to build a tree house !!\n(You can get wood from: fence, board, log, crate)</color> ";
         }
         else if (skipBtn.missionNum == 2)
         {
-            contentText.text = "If you have some trees,\n you need [objects to fix them].\n (You can fix tree with: Nail and Hammer, Wood)";
+            contentText.text = "<color=black>If you have some trees,\n you need [objects to fix them].\n (You can fix tree with: Nail and Hammer, Wood)</color>";
         }
         else if (skipBtn.missionNum == 3)
         {
-            contentText.text = "These are possible combinations.\n tree + rope \n tree + nail + hammer \n tree + rope + leaf \n=> nail and hammer should be together to use.";
+            contentText.text = "<color=black>These are possible combinations.\n tree + rope \n tree + nail + hammer \n tree + rope + leaf \n=> nail and hammer should be together to use.</color>";
         }
         else
         {
@@ -38,15 +39,15 @@ public class hintMessage : MonoBehaviour
         leveltext.text = "Normal Hint";
         if (skipBtn.missionNum == 1)
         {
-            contentText.text = "If you have some trees,\n you need [objects to fix them].\n (You can fix tree with: Nail and Hammer, Wood)";
+            contentText.text = "<color=black>If you have some trees,\n you need [objects to fix them].\n (You can fix tree with: Nail and Hammer, Wood)</color>";
         }
         else if (skipBtn.missionNum == 2)
         {
-            contentText.text = "If you have some trees, you need [objects to fix them[.\n (You can fix tree with: Nail and Hammer, Wood)";
+            contentText.text = "<color=black>If you have some trees, you need [objects to fix them[.\n (You can fix tree with: Nail and Hammer, Wood)</color>";
         }
         else if (skipBtn.missionNum == 3)
         {
-            contentText.text = "These are possible combinations.\n tree + rope \n tree + nail + hammer \n tree + rope + leaf \n=> nail and hammer should be together to use.";
+            contentText.text = "<color=black>These are possible combinations.\n tree + rope \n tree + nail + hammer \n tree + rope + leaf \n=> nail and hammer should be together to use.</color>";
         }
         else
         {
@@ -61,15 +62,15 @@ public class hintMessage : MonoBehaviour
         leveltext.text = "Hard Hint";
         if (skipBtn.missionNum == 1)
         {
-            contentText.text = "These are possible combinations.\n* tree + rope \n*  tree + nail + hammer \n*  tree + rope + leaf \n=> nail and hammer should be together to use.";
+            contentText.text = "<color=black>These are possible combinations.\n* tree + rope \n*  tree + nail + hammer \n*  tree + rope + leaf \n=> nail and hammer should be together to use.</color>";
         }
         else if (skipBtn.missionNum == 2)
         {
-            contentText.text = "If you have some trees,\n you need [objects to fix them].\n (You can fix tree with: Nail and Hammer, Wood)";
+            contentText.text = "<color=black>If you have some trees,\n you need [objects to fix them].\n (You can fix tree with: Nail and Hammer, Wood)</color>";
         }
         else if (skipBtn.missionNum == 3)
         {
-            contentText.text = "These are possible combinations.\n* tree + rope \n*  tree + nail + hammer \n*  tree + rope + leaf \n=> nail and hammer should be together to use.";
+            contentText.text = "<color=black>These are possible combinations.\n* tree + rope \n*  tree + nail + hammer \n*  tree + rope + leaf \n=> nail and hammer should be together to use.</color>";
         }
         else
         {
